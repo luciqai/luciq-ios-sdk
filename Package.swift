2c4e77bf4ebf377712ec794a5765284980654d3e
Instabug-iOS-CI <github-ci+ios@instabug.com>
2026-03-09 03:22:37 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Luciq",
-            url: "https://github.com/luciqai/luciq-ios-sdk/releases/download/19.5.0/Luciq-XCFramework.zip",
-            checksum: "cc24fd635b692ceb008700fe4f003377088096956e1da23ffc37a9337d17ba5d")
+            url: "https://ios-releases.luciq.ai/custom_spm/fix-app-hangs-stressing-nonfatals-flutter/19.5.0/Luciq/archive.zip",
+            checksum: "b79c0f64aa66f66df35cb862dbb0132e56f164e59873ee54f335c9d815abcb39")
     ]
 )
