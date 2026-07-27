e896f6aa8ec167384c115393251f03efb6eb9978
Instabug-iOS-CI <github-ci+ios@instabug.com>
2026-07-27 13:16:47 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Luciq",
-            url: "https://github.com/luciqai/luciq-ios-sdk/releases/download/19.9.2/Luciq-XCFramework.zip",
-            checksum: "7a2f08ebdd40356547b6bf9e53b1aed5e7a3d9d49608f43355d859cde624e271")
+            url: "https://ios-releases.luciq.ai/custom_spm/fix-swt-365-backport-unwire-user-steps/19.9.2/Luciq/archive.zip",
+            checksum: "9c727e075654ed61d30b7444c6e96ebadeee42072bc74845d674ee36a13d360f")
     ]
 )
