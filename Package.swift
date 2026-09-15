66fc19cc0f16f6cf0dfd7f2fbe8d2b2a12a93259
Instabug-iOS-CI <github-ci+ios@instabug.com>
2026-09-15 08:44:30 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Luciq",
-            url: "https://github.com/luciqai/luciq-ios-sdk/releases/download/19.10.1/Luciq-XCFramework.zip",
-            checksum: "27e2fe79455666c5ff9d576a1f78ca0942bca710eb056e25026ad5c307f9259e")
+            url: "https://ios-releases.luciq.ai/custom_spm/chore-webview-network-logs-flow-diagnostic-logs/19.10.1/Luciq/archive.zip",
+            checksum: "af1ed1c790d40e66acbbc8d1cb855ee161cdb655a600806903392c6ba3d696de")
     ]
 )
