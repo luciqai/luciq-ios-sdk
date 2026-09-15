fdf3951e64f5f3f373d4ffe788820afac54214fe
Instabug-iOS-CI <github-ci+ios@instabug.com>
2026-09-15 07:44:11 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Luciq",
-            url: "https://github.com/luciqai/luciq-ios-sdk/releases/download/19.10.1/Luciq-XCFramework.zip",
-            checksum: "27e2fe79455666c5ff9d576a1f78ca0942bca710eb056e25026ad5c307f9259e")
+            url: "https://ios-releases.luciq.ai/custom_spm/fix-webview-network-logs-swt-214-remaining-capture-gaps/19.10.1/Luciq/archive.zip",
+            checksum: "8cee463aaa3598943afc720364a2287765a5e9d68f20cd2a73002ba7331d5813")
     ]
 )
